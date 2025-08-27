bcd28590d3c74ef163f05946dc11e4cf0cfbfdf2
Joeward Peralta <joeward_peralta@outlook.com>
2025-08-27 13:56:21 -0400

    Add more spaces for the title component

--- src/components/typography/Title.tsx
+++ src/components/typography/Title.tsx
@@ -8,7 +8,7 @@ type TitleProps = {
 export default function Title({ className, children }: TitleProps) {
   return (
     <h1
-      className={`text-secondary text-3xl font-medium mb-4 md:mb-6 md:text-6xl ${
+      className={`text-secondary text-3xl font-medium mb-4 md:mb-6 md:text-6xl md:leading-16 ${
         className ?? ""
       }`}
     >
