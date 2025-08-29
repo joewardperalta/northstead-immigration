7df9692e3b1018309a908e7f53312955262e6796
Joeward Peralta <joeward_peralta@outlook.com>
2025-08-28 22:54:33 -0400

    Update the size and margins for smooth transition from mobile to desktop.

--- src/components/typography/Title.tsx
+++ src/components/typography/Title.tsx
@@ -8,7 +8,7 @@ type TitleProps = {
 export default function Title({ className, children }: TitleProps) {
   return (
     <h1
-      className={`text-secondary text-3xl font-medium mb-4 md:mb-6 md:text-6xl md:leading-16 ${
+      className={`text-secondary text-4xl font-medium mb-4 leading-11 sm:text-5xl sm:mb-5 sm:leading-14 md:text-6xl md:mb-6 md:leading-[4.25rem] ${
         className ?? ""
       }`}
     >
